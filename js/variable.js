c827ef747619236e70f5b119a5b1247a381c0ebb
czq <18663104463@163.com>
2017-07-13 14:56:10 +0800

    bb

--- js/variable.js
+++ js/variable.js
@@ -1,13 +1,13 @@
-// var register_url = 'http://139.199.157.185/WebChat/ajax/register.php';
-// var chat_url = 'http://139.199.157.185/WebChat/ajax/chat.php';
-// var login_url = 'http://139.199.157.185/WebChat/ajax/login.php';
-// var getuser_url = 'http://139.199.157.185/WebChat/ajax/getuser.php';
-// var getlist_url = 'http://139.199.157.185/WebChat/ajax/list.php';
-// var logout_url = 'http://139.199.157.185/WebChat/ajax/logout.php';
+var register_url = 'http://139.199.157.185/WebChat/ajax/register.php';
+var chat_url = 'http://139.199.157.185/WebChat/ajax/chat.php';
+var login_url = 'http://139.199.157.185/WebChat/ajax/login.php';
+var getuser_url = 'http://139.199.157.185/WebChat/ajax/getuser.php';
+var getlist_url = 'http://139.199.157.185/WebChat/ajax/list.php';
+var logout_url = 'http://139.199.157.185/WebChat/ajax/logout.php';
 
-var register_url = 'ajax/register.php';
-var chat_url = 'ajax/chat.php';
-var login_url = 'ajax/login.php';
-var getuser_url = 'ajax/getuser.php';
-var getlist_url = 'ajax/list.php';
-var logout_url = 'ajax/logout.php';
+// var register_url = 'ajax/register.php';
+// var chat_url = 'ajax/chat.php';
+// var login_url = 'ajax/login.php';
+// var getuser_url = 'ajax/getuser.php';
+// var getlist_url = 'ajax/list.php';
+// var logout_url = 'ajax/logout.php';
