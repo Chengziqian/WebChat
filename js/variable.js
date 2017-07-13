9c72c35147ab54cafcdf46355dd6ea0783dc882c
czq <18663104463@163.com>
2017-07-13 14:46:20 +0800

    aa

--- js/variable.js
+++ js/variable.js
@@ -1,3 +1,10 @@
+// var register_url = 'http://139.199.157.185/WebChat/ajax/register.php';
+// var chat_url = 'http://139.199.157.185/WebChat/ajax/chat.php';
+// var login_url = 'http://139.199.157.185/WebChat/ajax/login.php';
+// var getuser_url = 'http://139.199.157.185/WebChat/ajax/getuser.php';
+// var getlist_url = 'http://139.199.157.185/WebChat/ajax/list.php';
+// var logout_url = 'http://139.199.157.185/WebChat/ajax/logout.php';
+
 var register_url = 'ajax/register.php';
 var chat_url = 'ajax/chat.php';
 var login_url = 'ajax/login.php';
